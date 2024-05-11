bd619affeab8106a335bc29b4e9978d0cf64ae15
corruptvision <3matoro45@gmail.com>
2024-05-11 19:27:03 -0400

    changed reliance on puppeteer to a python wrapper. It is outdated, so I will try a new solution later, but the scripts work as they are

--- overreliance/overreliance_data_sanitizer.py
+++ overreliance/overreliance_data_sanitizer.py
@@ -1,6 +1,9 @@
 from rake_nltk import Rake
 import subprocess
 
+import asyncio
+from pyppeteer import launch
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -67,6 +70,77 @@ class OverrelianceDataSanitizer():
             
             #generating a list of links from the result of the script
             link_list = result.stdout.split()
+            
+            #dictionary to be converted to JSON
+            data = dict()
+            data["score"] = pair[0]
+            data["keyphrase"] = term
+            if not link_number_limit:
+                data["links"] = link_list
+            else:
+                data["links"] = link_list[:link_number_limit]
+            
+            keyphrase_data_list.append(data)
+        
+        return keyphrase_data_list
+    
+    async def search_and_scrape(self,link,search):
+        # Launch a new browser instance
+        
+        
+        browser = await launch(executablePath='C:\\Users\\crossfire234\\Desktop\\WorkStuff\\BCAMP\\AiShields\\Win_x64_1000027_chrome-win\\chrome-win\\chrome.exe')
+        
+        # Open a new page
+        page = await browser.newPage()
+        
+        # Navigate to a website
+        await page.goto(link)
+        
+        await page.type('textarea',search,{'delay':100})
+        
+        await page.keyboard.press('Enter', {'delay':100})
+        
+        await page.waitForNavigation()
+        
+        await page.screenshot({'path': 'google1.png', 'delay': 100})
+        
+        # Click an element on the page
+        data = await page.evaluate('''() => {
+        const search = document.querySelectorAll('a');
+        const urls = Array.from(search).map(v => v.href);
+        return urls;
+    }''')
+        
+        # Close the browser
+        await browser.close()
+        
+        filtered_data = [item for item in data if 'google' not in item and item != '']
+        return filtered_data
+    
+    def get_keyphrases_and_links(self,prompt_text, search_number_limit,link_number_limit = None, stopword_list =[]):
+        
+        print(search_number_limit)
+    
+        rake = Rake()
+        
+        #get the unique list of phrases
+        rake.extract_keywords_from_text(prompt_text)
+        keyword_phrase = list(set(rake.get_ranked_phrases_with_scores()))
+        
+        #sort by the score (highest)
+        sorted_keyphrases = sorted(keyword_phrase, key= lambda x: x[0], reverse=True)
+        
+        keyphrase_data_list = []
+        
+        #it either the length of the number of keyphrases, or the desired search list. Because of the sorting, higher scored phrases are searched first
+        for pair in sorted_keyphrases[:min(len(sorted_keyphrases), search_number_limit)]:
+            
+            term = self._remove_special_chars(pair[1],stopword_list)
+            
+            print("performing search on keyword:",term)
+            
+            #generating a list of links from the result of the script
+            link_list = asyncio.run(self.search_and_scrape('https://www.google.com',term))
             
             #dictionary to be converted to JSON
             data = dict()
