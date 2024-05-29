97353ec416ace9c160d5b9e793fec78808681902
Patrick Kelly <167281535+aishields@users.noreply.github.com>
2024-05-28 23:06:43 -0500

    Update overreliance_data_sanitizer.py

--- overreliance/overreliance_data_sanitizer.py
+++ overreliance/overreliance_data_sanitizer.py
@@ -1,7 +1,11 @@
 from rake_nltk import Rake
 import nltk
-import subprocess
+import re
 
+try:
+    import key2vec.key2vec as key2vec
+except:
+    import overreliance.key2vec.key2vec as key2vec
 
 nltk.download('punkt')
 
@@ -25,7 +29,8 @@ current_path = os.getcwd()
 
 class OverrelianceDataSanitizer():
     def __init__(self):
-        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        self.headers_old = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.118 Safari/537.3'}
         self.chrome_path  = "C:/Users/crossfire234/Desktop/Win_x64_1000027_chrome-win/chrome-win/chrome.exe"
     
     ''' get_keyphrases '''
@@ -77,7 +82,7 @@ class OverrelianceDataSanitizer():
             await page.wait_for_load_state('networkidle')
 
             # Take a screenshot
-            await page.screenshot(path='google1.png')
+            #await page.screenshot(path='google1.png')
 
             # Click an element on the page. Perhaps link filtering should go here
             data = await page.evaluate('''() => {
@@ -92,15 +97,222 @@ class OverrelianceDataSanitizer():
             # Filter the data
             filtered_data = [item for item in data if 'google' not in item and item != '']
             return filtered_data
+        
+    def search_and_scrape_2(self, link, search_string, delay=0, timeout=5):
+        
+        query_link = link + f'/search?q={search_string}'
+        response = requests.get(query_link, headers=self.headers, timeout=timeout)
+        
+        time.sleep(delay)
+        
+        print("response:",response.content)
+        html_content = response.content
+        
+
+        #quick and dirty, but not great
+        soup = BeautifulSoup(html_content, 'html.parser')
+        a_tags = soup.find_all('a')
+        links = [tag['href'] for tag in a_tags]
+        
+        links = [link for link in links if 'google' not in link and link != '']
+        
+        links = [link for link in links if 'https://' in link]
+        
+        final_links = []
+        for link in links:
+            if 'url=' in link:
+                index = link.find('url=')
+                final_links.append(link[index+4:])
+            
+        return final_links
+     
+    def _group_and_combine(self,lst):
+    # Create a dictionary to group the strings by their scores
+        score_dict = {}
+        for score, string in lst:
+            if score in score_dict:
+                score_dict[score].append(string)
+            else:
+                score_dict[score] = [string]
+
+        # Create a new list with summed scores and concatenated strings
+        new_lst = []
+        for score, strings in score_dict.items():
+            combined_string = ' '.join(strings)
+            new_lst.append([sum([score]), combined_string])
+
+        return new_lst   
     
+    def _parse_sentences(self,input_text,cutoff_parameter=10):
+        sentence_ends = re.finditer(r'[.!?]+(?=\s+|$)',input_text)
+        
+        sentence_list =[]
+        
+        indices = []
+        indices.append(0)
+        for punc_match in sentence_ends:
+            print(punc_match.start())
+            indices.append(punc_match.start()+1)
+            
+        print("indices:")
+        print(indices)
+                
+        for i in range(len(indices)-1):
+            print(input_text[indices[i]:indices[i+1]])
+            
+            sentence_list.append(input_text[indices[i]:indices[i+1]])
+            
+        if len(sentence_list) == 0:
+            sentence_list.append(input_text)
+        
+        return sentence_list
+      
+      
+    def get_keyphrases_key2vec(self, prompt_text,stopword_list=[],keyphrase_depth=1):
+        pass
+              
+    def get_keyphrases(self, prompt_text, stopword_list = [],keyphrase_depth=1,method='rake',spacy_nlp = None):
+        
+        print("Input prompt:")
+        print(prompt_text)
+        print()
+        
+        if method == 'rake':
+            rake = Rake()
+            cleaned_prompt_text = self.remove_special_sentences(prompt_text)
+            
+            print("cleaned input text:")
+            print(cleaned_prompt_text)
+                
+            rake.extract_keywords_from_text(cleaned_prompt_text)
+            keyword_phrases = list(set(rake.get_ranked_phrases_with_scores()))
+                
+            ''' word score: deg(w)/feq(w)'''
+            ''' deg is the number of co-occurrences of a word in a piece of text. This is the sum of the number of times the word appears in the text, plus the number times the particular word appears next to a different word, which are not stop words, appear next'''
+        
+        if method == 'key2vec':
+            
+            #I think this might be more efficient to get the embeddings that are directly
+            #relevant to the key2vec algorithm, but I'm not really sure. I believe the vectors should
+            #include the proper relationships to the rest of the english corpus
+            glove = key2vec.glove.Glove(spacy_nlp=spacy_nlp,text = prompt_text)
+            m = key2vec.key2vec.Key2Vec(prompt_text,glove)
+            m.extract_candidates()
+            m.set_theme_weights()
+            m.build_candidate_graph()
+            ranked = m.page_rank_candidates()
+            keyphrases = [phrase.text for phrase in ranked]
+            print("key2vec keyphrases:")
+            print(keyphrases)
+            keyphrases = list(set(keyphrases))
+            
+            keyword_phrases = [[len(keyphrases),' '.join(keyphrases)]]
+            
+        
+
+        sorted_keyphrases = sorted(keyword_phrases, key = lambda x: x[0],reverse=True)
+        
+        
+        
+        sorted_keyphrases = list(map(lambda x: list(x),sorted_keyphrases))
+        for i, pair in enumerate(sorted_keyphrases):
+            term = self._remove_special_chars(pair[1],stopword_list)
+            sorted_keyphrases[i][1] = term
+        
+        print("Raw Sorted Keyphrases:")
+        print(sorted_keyphrases)
+        
+        final_sorted_keyphrases = self._group_and_combine(sorted_keyphrases)    
+        
+        print("Combined Sorted Keyphrases:")
+        print(final_sorted_keyphrases)  
+        
+        if keyphrase_depth > 1 and keyphrase_depth <= len(final_sorted_keyphrases):
+            summed_sorted_keyphrases = []
+            for i in range(0,len(final_sorted_keyphrases),keyphrase_depth):
+                if i + keyphrase_depth < len(final_sorted_keyphrases):
+                    group = final_sorted_keyphrases[i:i+keyphrase_depth]        
+                else:
+                    group = final_sorted_keyphrases[i:len(final_sorted_keyphrases)]
+                
+                group_string = ' '.join(item[1] for item in group)
+                summed_sorted_keyphrases.append([group[0][0],group_string])
+                
+                
+            '''data = list(list(float,str))'''
+         
+            print("Depth Combined Sorted Keyphrases")
+            print(summed_sorted_keyphrases)   
+        
+            return summed_sorted_keyphrases
+        else:
+             return final_sorted_keyphrases  
+    
+    def get_links(self,sorted_keyphrases, search_number_limit,  site_ignore_list=[],link_number_limit = None,new_search=False):
+        keyphrase_data_list=[]
+        for pair in sorted_keyphrases[:min(len(sorted_keyphrases), search_number_limit)]:
+            
+            term = pair[1]
+            
+            #generating a list of links from the result of the script
+            if not new_search:
+                link_list = asyncio.run(self.search_and_scrape('https://www.google.com',term))
+            if new_search:
+                link_list = self.search_and_scrape_2('https://www.google.com',term,delay=2)
+               
+            #this filters out any link in the site_ignore_list
+            link_list = [link for link in link_list if not any(ignore_link in link for ignore_link in site_ignore_list)]
+            
+            #this automatically removes duplicates
+            link_list = list(set(link_list))
+            
+            #dictionary to be converted to JSON
+            data = dict()
+            data["score"] = pair[0]
+            data["keyphrase"] = term
+            if not link_number_limit:
+                data["links"] = link_list
+            else:
+                data["links"] = link_list[:min(len(link_list), link_number_limit)]
+            
+            keyphrase_data_list.append(data)
+        
+        ''' data = list({score: float, keyphrase: string, links: list(str)})'''
+        
+        return keyphrase_data_list
+    
+    def remove_special_sentences(self,text):
+        # Split the text into sentences
+        sentences = re.split(r'[.!?][\s\n]+', text)
+        
+        print(sentences)
+
+        # Create a list to store the cleaned sentences
+        cleaned_sentences = []
+
+        for sentence in sentences:
+            # Check if the sentence contains a string in all caps within angled brackets
+            if not re.search(r'<[A-Z]+>', sentence) and not re.search(r'&lt;script&gt;.*?&lt;/script&gt;', sentence):
+                # Remove text between < and >
+                cleaned_sentences.append(sentence)
+                print("clean sentence:")
+                print()
+                print(sentence)
+
+        # Join the cleaned sentences back into a single string
+        cleaned_text = ' '.join(cleaned_sentences)
+
+        return cleaned_text
+        
     def get_keyphrases_and_links(self,prompt_text, search_number_limit,link_number_limit = None, stopword_list =[]):
         
         print(search_number_limit)
     
         rake = Rake()
         
+        cleaned_prompt_text = self.remove_special_sentences(prompt_text)
         #get the unique list of phrases
-        rake.extract_keywords_from_text(prompt_text)
+        rake.extract_keywords_from_text(cleaned_prompt_text)
         keyword_phrase = list(set(rake.get_ranked_phrases_with_scores()))
         
         #sort by the score (highest)
@@ -135,34 +347,36 @@ class OverrelianceDataSanitizer():
     
     ''' get_articles '''
     
-    def get_articles(self,keyphrase_data_list,auto_ignore_flag = True, site_ignore_list=[], timeout=5):
+    def get_articles(self,keyphrase_data_list,auto_ignore_flag = True, timeout=5):
         for i, data in enumerate(keyphrase_data_list):
             print()
             print("Keyphrase:",data["keyphrase"])
             print()
             
-            print(list(set(data["links"])))
+            
             for j, link in enumerate(data["links"]):
-                if all(ignored not in link for ignored in site_ignore_list):
+                
+                print("scraping:",link)
                     
-                    print(link)
-                    
-                    try:
-                        response = requests.get(link, headers=self.headers, timeout=timeout)
-                        html_content = response.content
+                try:
+                    response = requests.get(link, headers=self.headers, timeout=timeout)
+                    html_content = response.content
 
-                        #quick and dirty, but not great
-                        soup = BeautifulSoup(html_content, 'html.parser')
+                    #quick and dirty, but not great
+                    soup = BeautifulSoup(html_content, 'html.parser')
                     
-                        full_text = soup.text
-                        keyphrase_data_list[i]["links"][j] = {"link": link, "text": full_text}
+                    full_text = soup.text
+                    keyphrase_data_list[i]["links"][j] = {"link": link, "text": full_text}
                         
-                    except requests.exceptions.Timeout:
-                        keyphrase_data_list[i]["links"][j] = {"link": link, "text": None}
-                        print("Request timed out. Moving on...")
-       
-                else:
+                except requests.exceptions.Timeout:
                     keyphrase_data_list[i]["links"][j] = {"link": link, "text": None}
+                    print("Request timed out. Moving on...")
+                    continue
+                except Exception as e:
+                    keyphrase_data_list[i]["links"][j] = {"link": link, "text": None}
+                    print('A different error occurred. Moving on...')
+                    continue
+                
                     
         ''' data = list({score: float, keyphrase: string, links: list({link: str, text: str or None})})'''
                     
